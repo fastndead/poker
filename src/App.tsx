dd76dea794a2cdcd479d7cc3d51e20e18f7e4ff5
Asman Umbetov <asman.umbetov@sbermarket.ru>
2023-02-18 23:22:58 +0300

    fixed manifest

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 function App()  {
     return (
-        <h1 className="text-4xl font-bold underline">
-      Hello world!
-        </h1>
+        <div className="container mx-5">
+            <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">
+              Poker
+
+            </h1>
+
+        </div>
     )
 }
 
