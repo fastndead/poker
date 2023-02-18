3029869f16ba8758bfa6cf4619b7b98b35c2f9e7
Asman Umbetov <asman.umbetov@sbermarket.ru>
2023-02-18 23:16:57 +0300

    added tailwind added typescript to the client added eslint removed all the boilerplate CRA bullshit

---
+++ src/sockets/socket.ts
@@ -0,0 +1,6 @@
+import io from 'socket.io-client'
+
+const isDev = process.env.NODE_ENV !== 'production'
+const socketUrl = isDev ? 'http://localhost:8080' : '/'
+
+export const socket = io(socketUrl)
