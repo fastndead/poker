30281000a76916f8ccee2a0ba9804582d6f46757
Asman Umbetov <asman.umbetov@sbermarket.ru>
2023-03-21 22:53:10 +0300

    made animations for the cards better

--- src/pages/room/features/PlayingCardsInput.tsx/index.tsx
+++ src/pages/room/features/PlayingCardsInput.tsx/index.tsx
@@ -6,24 +6,41 @@ type Props = {
   cards: string[]
 }
 
-const MAX_Y_VALUE = 50
-const MAX_ROTATE_VALUE = 13
-const MAX_X_VALUE = 250
-function getValue(maxValue: number, isCenter:boolean, index: number, length: number, isSymmetrical: boolean):number {
+const MAX_Y_VALUE = 1
+const MAX_ROTATE_VALUE = 30
+const MAX_X_VALUE = 230
+function getValue({
+  maxValue,
+  isCenter,
+  index,
+  length,
+  isSymmetrical,
+  isAccelerated,
+  step
+} : {
+    maxValue: number,
+    isCenter:boolean,
+    index: number,
+    length: number,
+    isSymmetrical: boolean
+    isAccelerated?: boolean
+    step?: number
+  }) {
+
   if(isCenter) {
     return 0
   }
 
   const isLeftSide = length / 2 > index
-  const lengthOfHalf = Math.floor(length /2)
+  const lengthOfHalf = Math.floor(length / 2)
   const isOdd = length % 2 !== 0
   const absoluteIndex = isLeftSide
     ? lengthOfHalf - index + 1
     : isOdd ? index - lengthOfHalf - 1 : index - lengthOfHalf
 
-  const yStep = maxValue / lengthOfHalf
+  const yStep = step || maxValue / lengthOfHalf
 
-  const absoluteResult = absoluteIndex * yStep
+  const absoluteResult = isAccelerated ? absoluteIndex * yStep + ((absoluteIndex - 1) * 10) : absoluteIndex * yStep
   if(isSymmetrical) {
     return isLeftSide ? -absoluteResult : absoluteResult
   }
@@ -37,19 +54,31 @@ function getCardConfigFactory(selectedIndex: number, cardsAmount: number){
     const length = cardsAmount
     const isOdd = length % 2 !== 0
     const isCenter = isOdd && Math.ceil(length/2) === realIndex
-    const rotateValue = getValue(MAX_ROTATE_VALUE, isCenter, realIndex, length, true)
-    const xValue =  getValue(MAX_X_VALUE, isCenter, realIndex, length, true)
-    const yValue = getValue(MAX_Y_VALUE,isCenter, realIndex, length, false)
+
+    const rotateValue = getValue({ maxValue: MAX_ROTATE_VALUE, isCenter, index: realIndex, length, isSymmetrical: true, step: 5 })
+    const xValue =  getValue({ maxValue: MAX_X_VALUE, isCenter, index: realIndex, length, isSymmetrical: true })
+    const yValue = getValue({ maxValue: MAX_Y_VALUE, isCenter, index: realIndex, length, isSymmetrical: false, isAccelerated: true, step: 3 })
+
+    if(isSelected){
+      const coef = 30
+      
+      return {
+        y: yValue - (coef * Math.cos(rotateValue * (Math.PI/180))),
+        x: xValue + (coef * Math.cos((90 - rotateValue) * (Math.PI/180))),
+        rotate: rotateValue,
+        zIndex: index,
+      }
+    }
 
     return {
-      y: isSelected ? yValue - 30 : yValue,
+      y: yValue,
       x: xValue,
       rotate: rotateValue,
       zIndex: index,
     }
+
   }
 }
-
 
 export default function PlayingCardsInput({ cards }:Props) {
   const [springs, api] = useSprings(
@@ -79,7 +108,7 @@ export default function PlayingCardsInput({ cards }:Props) {
               onMouseEnter={handleHover(index)}
               onMouseLeave={handleHoverEnd()}
               key={index}
-              className={'absolute border cursor-pointer bg-white border-primary-emphasis rounded-lg shadow-card' + ' ' + index}
+              className={'absolute border cursor-pointer bg-white border-primary-emphasis rounded-lg shadow-card'}
               style={{
                 width: '100px',
                 height: '160px',
