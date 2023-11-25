7999d4b243ef49735fb8206b5c04961f9bc9d3c3
Umbetov Asman <33729448+fastndead@users.noreply.github.com>
2023-11-25 21:37:48 +0300

    fix(client & server): fixes bug with too many people in the room (#14)
    fix #6
    
    Co-authored-by: Asman Umbetov <asman.umbetov@sbermarket.ru>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,45 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
   ],
-  'overrides': [
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-    'eslint-plugin-react-hooks',
-    '@typescript-eslint',
-  ],
-  'rules': {
+  plugins: ['react', 'eslint-plugin-react-hooks', '@typescript-eslint'],
+  rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    'no-debugger':'off',
+    'no-debugger': 'off',
     'keyword-spacing': ['error'],
     'space-infix-ops': ['error'],
     '@typescript-eslint/quotes': [
       'error',
       'single',
       {
-        'avoidEscape': true,
-        'allowTemplateLiterals': true
-      }
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
     ],
     'jsx-quotes': ['error', 'prefer-single'],
     'react/jsx-first-prop-new-line': ['error', 'always'],
     'object-curly-spacing': ['error', 'always'],
-    'react/jsx-max-props-per-line': ['error', { 'maximum': 1, 'when': 'always' }],
-    'react/jsx-closing-bracket-location':  ['error', 'tag-aligned'],
+    'react/jsx-max-props-per-line': ['error', { maximum: 1, when: 'always' }],
+    'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
     'react/jsx-closing-tag-location': ['error'],
     'react/jsx-child-element-spacing': ['error'],
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ]
-  }
-}
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+  },
+};
--- src/pages/room/index.tsx
+++ src/pages/room/index.tsx
@@ -2,20 +2,33 @@ import Sidebar from 'components/Sidebar'
 import React, { useCallback, useEffect, useState } from 'react'
 import { DEFAULT_CARD_SYSTEM } from './constants'
 import PlayingCardsInput from './features/PlayingCardsInput.tsx'
+import { useNotifications } from 'hooks/useNotifications'
 import PlayingDesk from './features/PlayingDesk'
-import { Player, PlayerSeialized } from './types'
+import { Player } from './types'
 import { socket } from 'sockets/socket'
 import { useParams } from 'react-router-dom'
 import EnterNameModal from 'components/EnterNameModal'
 import { useLocalStorage } from 'hooks/useLocalStorage'
 
 export function Room() {
-  const { roomName } = useParams<{roomName: string}>()
+  const { roomName } = useParams<{ roomName: string }>()
   const [players, setPlayers] = useState<Player[]>([])
   const [isRevealed, setIsRevealed] = useState<boolean>(false)
+  const [spectatorMode, setSpectatorMode] = useState<boolean>(false)
+  const { addNotification } = useNotifications()
 
   const [name, setName] = useLocalStorage<string | null>('userName', null)
+  const [id, setId] = useLocalStorage<string | null>('userId', null)
   const [isNameModalVisible, setIsNameModalVisible] = useState(false)
+
+  useEffect(() => {
+    if (spectatorMode) {
+      addNotification({
+        type: 'error',
+        text: 'This room is full right now, you joined as a spectator',
+      })
+    }
+  }, [addNotification, spectatorMode])
 
   useEffect(() => {
     if (!name) {
@@ -24,29 +37,27 @@ export function Room() {
       socket.emit('mynameis', { name })
       socket.emit('join', { roomName })
 
-      socket.on('initial_state', ({ players }: {players: PlayerSeialized[]}) => {
-        const deserializedPlayers = players.map((player) => ({
-          name: player.name,
-          id: player.id,
-          value: player.value
-        }))
-        setPlayers([...deserializedPlayers] )
+      socket.on(
+        'initial_state',
+        ({ players, id, spectator }: { players: Player[]; spectator: boolean; id: string }) => {
+          setPlayers(players)
+          setSpectatorMode(spectator)
+          setId(id)
+        }
+      )
+
+      socket.on('update', ({ players }: { players: Player[] }) => {
+        setPlayers(players.filter((player) => player.id !== id))
       })
 
-      socket.on('new_player', ({ player }: {player: Player}) => {
-      
-        setPlayers((prev) => {
-          return [...prev, player]})
-      })
-
-      socket.on('player_disconnect', ({ playerId }: {playerId: string}) => {
-        setPlayers((prev) => [...prev].filter((player)=> player.id !== playerId))
+      socket.on('player_disconnect', ({ playerId }: { playerId: string }) => {
+        setPlayers((prev) => [...prev].filter((player) => player.id !== playerId))
         setIsRevealed(false)
       })
 
-      socket.on('player_voted', ({ value, playerId }: {value: string, playerId: string}) => {
+      socket.on('player_voted', ({ value, playerId }: { value: string; playerId: string }) => {
         setPlayers((prev) => {
-          return [...prev].map((player)=> player.id === playerId ? { ...player, value } : player)
+          return [...prev].map((player) => (player.id === playerId ? { ...player, value } : player))
         })
       })
 
@@ -61,8 +72,7 @@ export function Room() {
         })
         setUserValue(null)
       })
-    } 
-
+    }
 
     return () => {
       socket.emit('leave', { roomId: roomName })
@@ -71,10 +81,13 @@ export function Room() {
 
   const [userValue, setUserValue] = useState<Player['value']>(null)
 
-  const handleUserCardChange = useCallback((value: Player['value']) => {
-    socket.emit('vote', { voteValue: value })
-    setUserValue(value)
-  }, [setUserValue])
+  const handleUserCardChange = useCallback(
+    (value: Player['value']) => {
+      socket.emit('vote', { voteValue: value })
+      setUserValue(value)
+    },
+    [setUserValue]
+  )
 
   const handleShowAll = useCallback(() => {
     socket.emit('show_all')
@@ -83,7 +96,6 @@ export function Room() {
   const handleReset = useCallback(() => {
     socket.emit('reset')
   }, [])
-
 
   return (
     <div
@@ -100,13 +112,15 @@ export function Room() {
           onShowAll={handleShowAll}
           onReset={handleReset}
         />
-        <PlayingCardsInput
-          onChange={handleUserCardChange}
-          userValue={userValue}
-          cards={DEFAULT_CARD_SYSTEM}
-        />
+        {!spectatorMode && (
+          <PlayingCardsInput
+            onChange={handleUserCardChange}
+            userValue={userValue}
+            cards={DEFAULT_CARD_SYSTEM}
+          />
+        )}
       </div>
-      <EnterNameModal 
+      <EnterNameModal
         isVisible={isNameModalVisible}
         onClose={(inputName: string) => {
           setIsNameModalVisible(false)
@@ -115,5 +129,4 @@ export function Room() {
       />
     </div>
   )
-
 }
